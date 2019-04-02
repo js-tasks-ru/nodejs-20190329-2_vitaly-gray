f0575c769fa83a853457858d14b87ded82af45b9
l1a7t <l1a7tb@gmail.com>
2019-04-02 11:00:19 +0300

    revert solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,10 +1,9 @@
 function sum(a, b) {
   /* ваш код */
-    if(typeof a === 'number' && typeof b === 'number'){
-        return a + b;
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        throw new TypeError();
     }
-
-    throw new TypeError();
+    return a + b;
 }
 
 module.exports = sum;
